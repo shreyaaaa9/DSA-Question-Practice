7f3005936354631f22c794536c90237a743e76e1
Shreya Pramod Survase <shreyalalalala9@gmail.com>
2026-07-07 22:30:55 +0530

    Add Calculator practice question

---
+++ Question1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Calculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter num2:");
+        int num2 = sc.nextInt();
+        System.out.println("Enter operator:");
+        char operator = sc.next().charAt(0);
+        switch (operator){
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+
+        
+
+
+    }
+}
+        
